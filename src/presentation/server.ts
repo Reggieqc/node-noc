c319bcf3d850fecc692e9318a9788fdd939cf42e
Reggie Quiroz <regqc@hotmail.com>
2026-06-02 17:02:25 -0300

    Config node emailer and send email service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,6 +2,7 @@ import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+import { EmailService } from "./email/email.service";
 
 const fileSystemLogRepository = new LogRepositoryImpl(
   new FileSystemDataSource(),
@@ -11,7 +12,15 @@ export class Server {
   public static start() {
     console.log("Server started...");
     //Send email
+    // const emailService = new EmailService();
+    // emailService.sendEmail({
+    //   to: "regg.qc94@gmail.com",
+    //   subject: "Test email",
+    //   htmlBody: "<h1>This is a test email</h1>",
+    // });
+    // emailService.sendEmailWithAttachment("regg.qc94@gmail.com");
 
+    //Cron
     // CronService.createJob("*/5 * * * * *", () => {
     //   const url = "http://localhost:3000";
     //   new CheckService(
