1d1f36e994155fbc9896264986406e65e69b78b1
Reggie Quiroz <regqc@hotmail.com>
2026-06-02 12:09:53 -0300

    data source

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -1,4 +1,4 @@
-enum LogSeverityLevel {
+export enum LogSeverityLevel {
   LOW = "low",
   MEDIUM = "medium",
   HIGH = "high",
@@ -8,4 +8,17 @@ export class LogEntity {
   public level: LogSeverityLevel;
   public message: string;
   public createdAt: Date;
+
+  constructor(message: string, level: LogSeverityLevel) {
+    this.message = message;
+    this.level = level;
+    this.createdAt = new Date();
+  }
+
+  static fromJson(json: string) {
+    const { message, level, createdAt } = JSON.parse(json);
+    const log = new LogEntity(message, level);
+    log.createdAt = new Date(createdAt);
+    return log;
+  }
 }
---
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -0,0 +1,62 @@
+import { LogDataSource } from "../../domain/datasources/log.datasource";
+import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
+import fs from "fs";
+
+export class FileSystemDataSource implements LogDataSource {
+  private readonly logPath = "/logs";
+  private readonly allLogsPath = `${this.logPath}/logs-low.log`;
+  private readonly mediumLogsPath = `${this.logPath}/logs-medium.log`;
+  private readonly highLogsPath = `${this.logPath}/logs-high.log`;
+
+  constructor() {
+    this.createLogFiles();
+  }
+
+  private createLogFiles() {
+    [this.allLogsPath, this.mediumLogsPath, this.highLogsPath].forEach(
+      (path) => {
+        if (!fs.existsSync(path)) {
+          fs.writeFileSync(path, "");
+        }
+      },
+    );
+  }
+
+  getLogsFromFile(path: string): LogEntity[] {
+    const content = fs.readFileSync(path, "utf-8");
+    const logs = content
+      .split("\n")
+      .filter((line) => line.trim() !== "")
+      .map(LogEntity.fromJson);
+    return logs;
+  }
+
+  async saveLog(log: LogEntity): Promise<void> {
+    const logAsJson = `${JSON.stringify(log)}\n`;
+    switch (log.level) {
+      case LogSeverityLevel.LOW:
+        fs.appendFileSync(this.allLogsPath, logAsJson);
+        break;
+      case LogSeverityLevel.MEDIUM:
+        fs.appendFileSync(this.mediumLogsPath, logAsJson);
+        break;
+      case LogSeverityLevel.HIGH:
+        fs.appendFileSync(this.highLogsPath, logAsJson);
+        break;
+      default:
+        throw new Error("Invalid log level");
+    }
+  }
+  async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+    switch (severityLevel) {
+      case LogSeverityLevel.LOW:
+        return Promise.resolve(this.getLogsFromFile(this.allLogsPath));
+      case LogSeverityLevel.MEDIUM:
+        return Promise.resolve(this.getLogsFromFile(this.mediumLogsPath));
+      case LogSeverityLevel.HIGH:
+        return Promise.resolve(this.getLogsFromFile(this.highLogsPath));
+      default:
+        throw new Error("Log severity level not supported");
+    }
+  }
+}
